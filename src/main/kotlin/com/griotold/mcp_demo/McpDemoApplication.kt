88fafc540e0216a68de5e5aebcd468325639f087
Griotold <gotjd9773@naver.com>
2025-05-21 10:42:48 +0900

    Initial commit

---
+++ src/main/kotlin/com/griotold/mcp_demo/McpDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.griotold.mcp_demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class McpDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<McpDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/griotold/mcp_demo/McpDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.griotold.mcp_demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class McpDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
